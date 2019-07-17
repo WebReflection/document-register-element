88b066c28f3ea831b82d137a0acf15badeef86e2
morance <morance79@163.com>
2019-07-17 15:02:53 +0800

    chore(): reg refactor

--- src/document-register-element.js
+++ src/document-register-element.js
@@ -35,7 +35,7 @@ var
   PREFIX_IS = '=',
 
   // valid and invalid node names
-  validName = /^[A-Z](\.|-|_|[A-Z0-9])*(?:-)(\.|-|_|[A-Z0-9])*$/,
+  validName = /^[A-Z][-._A-Z0-9]*-[-._A-Z0-9]*$/,
   invalidNames = [
     'ANNOTATION-XML',
     'COLOR-PROFILE',
